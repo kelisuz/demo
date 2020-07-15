0e05df2a7c3f3f5c2e74ce91c1a876104b85a123
kelibryan <kelibryan@gmail.com>
2020-07-15 17:44:16 -0400

    Updates

---
+++ precache-manifest.409cbb8a52967f41718a55b49a96b694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db07c2bdffa7f5f8be0335b1da413b5",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "9e28a2b84e42c925e0ba",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b5e47673ac35fcd1ba9",
+    "url": "/demo/static/js/2.260e09e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.260e09e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e28a2b84e42c925e0ba",
+    "url": "/demo/static/js/main.59fdb706.chunk.js"
+  },
+  {
+    "revision": "d5c5a116ea50a9c29ed9",
+    "url": "/demo/static/js/runtime-main.9b8c710b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
